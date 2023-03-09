f5b59f22681e1bcadac85c04f70462e672875ab2
Leahnicoles <lnsimonn@hotmail.com>
2023-03-09 23:49:09 +0000

    add sell cake button and add extensions

--- src/components/Cake.js
+++ src/components/Cake.js
@@ -2,13 +2,19 @@
 
 const Cake = ({title, ingredients, price, rating}) => {
 
+    //Sell Button
+    //const handleSell = () => {
+        //onButtonClick(count + 1);
+    //}
+
     return(
         <>
             <h3>{title} Cakes </h3>
             <p>Ingredients: {ingredients} </p>
-            <p>Price: {price} </p>
+            <p>Price: £{price} </p>
             <p>Rating: {rating} </p>
             <p>Current total: 0</p>
+            <button>Sell Cake</button>
         </>
     )
 
--- src/containers/CakeContainer.js
+++ src/containers/CakeContainer.js
@@ -2,19 +2,24 @@
 import {useState} from 'react';
 import Cake from "../components/Cake";
 
+//Function CakeSalesTotal
 
 const CakeContainer = () => {
 
     const [victoriaSpongeCount, setVictoriaSpongeCount] = useState(0);
     const [teaLoafCount, setTeaLoafCount] = useState(0);
     const [carrotCakeCount, setCarrotCakeCount] = useState(0);
+    const [cakeSalesTotal, setCakeSalesTotal] = useState(0);
+
 
     return(
         <>
             <h2>Cakes: </h2>
             <Cake title={"Victoria Sponge"} ingredients={"butter, sugar, self-raising flour, baking powder, milk"} price={5} rating={5}/>       
             <Cake title={"Tea Loaf"} ingredients={"eggs, oil, dried fruit, sugar, self-raising flour, strong tea"} price={2} rating={3}/>      
-            <Cake title={"Carrot Cake"} ingredients={"carrots, walnuts, oil, cream cheese, flour, sugar"} price={8} rating={5}/> 
+            <Cake title={"Carrot Cake"} ingredients={"carrots, walnuts, oil, cream cheese, flour, sugar"} price={8} rating={5}/>
+            <p>Total Cakes Sold: {victoriaSpongeCount + teaLoafCount + carrotCakeCount}</p>
+            <p>Total Value of Cakes: {cakeSalesTotal}</p>
             <p>Total Average Ratings: Coming soon!</p>
         </>
     )
